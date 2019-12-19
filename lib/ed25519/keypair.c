d09c22290c0bfa3706df15e6b376c272d81804e6
Ckristian Duran <ckdur.iso@gmail.com>
2019-12-20 01:05:23 +0900

    ED25519 software restored

--- lib/ed25519/keypair.c
+++ lib/ed25519/keypair.c
@@ -1,30 +1,17 @@
 #include "ed25519.h"
 #include "hwsha3.h"
 #include "ge.h"
-#include "sifive/platform.h"
 
 
 void ed25519_create_keypair(unsigned char *public_key, unsigned char *private_key, const unsigned char *seed) {
-    //uint64_t *k = (uint64_t*)seed;
-    uint32_t *pub = (uint32_t*)public_key;
-    uint32_t *priv = (uint32_t*)private_key;
+    ge_p3 A;
 
     hwsha3_init();
     hwsha3_final(private_key, seed, 32);
-    
-     for(int i = 0; i < 8; i++) {
-        ED25519_REG(ED25519_REG_ADDR_K) = i;
-        if(i == 0) // TODO: This is really necessary? 
-            ED25519_REG(ED25519_REG_DATA_K) = *(priv+i) & 0xFFFFFFF8;
-        else if(i == 7)
-            ED25519_REG(ED25519_REG_DATA_K) = (*(priv+i) & 0x3FFFFFFF) | 0x40000000;
-        else
-            ED25519_REG(ED25519_REG_DATA_K) = *(priv+i);
-    }
-    ED25519_REG(ED25519_REG_STATUS) = 1; // Use the K memory
-    while(!(ED25519_REG(ED25519_REG_STATUS) & 0x4)); // Wait
-    for(int i = 0; i < 8; i++) {
-        ED25519_REG(ED25519_REG_ADDR_QY) = i;
-        pub[i] = ED25519_REG(ED25519_REG_DATA_QY);
-    }
+    private_key[0] &= 248;
+    private_key[31] &= 63;
+    private_key[31] |= 64;
+
+    ge_scalarmult_base(&A, private_key);
+    ge_p3_tobytes(public_key, &A);
 }
--- lib/ed25519/sign.c
+++ lib/ed25519/sign.c
@@ -3,49 +3,28 @@
 #include "hwsha3.h"
 #include "ge.h"
 #include "sc.h"
-#include "sifive/platform.h"
+
 
 void ed25519_sign(unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key, const unsigned char *private_key) {
+    //sha3_ctx_t hash;
     unsigned char hram[64];
     unsigned char r[64];
-    uint32_t *sig = (uint32_t*)signature;
+    ge_p3 R;
 
 
     hwsha3_init();
     hwsha3_update(private_key + 32, 32);
     hwsha3_final(r, message, message_len);
-    sc_reduce(r);
-    
-    for(int i = 0; i < 8; i++) {
-        ED25519_REG(ED25519_REG_ADDR_K) = i;
-        ED25519_REG(ED25519_REG_DATA_K) = *(((uint32_t*)(r)) + i);
-    }
-    ED25519_REG(ED25519_REG_STATUS) = 1; // Use the K memory
-    while(!(ED25519_REG(ED25519_REG_STATUS) & 0x4)); // Wait
-    for(int i = 0; i < 8; i++) {
-        ED25519_REG(ED25519_REG_ADDR_QY) = i;
-        sig[i] = ED25519_REG(ED25519_REG_DATA_QY);
-    }
 
-    // Calculate the H(R, A, M)
+    sc_reduce(r);
+    ge_scalarmult_base(&R, r);
+    ge_p3_tobytes(signature, &R);
+
     hwsha3_init();
     hwsha3_update(signature, 32);
     hwsha3_update(public_key, 32);
     hwsha3_final(hram, message, message_len);
 
-    // Calculate the S part with the addmult in hw
-    for(int i = 0; i < 16; i++) {
-        ED25519_REG(ED25519_REG_DATA_HKEY + i*4) = *(((uint32_t*)(private_key)) + i);
-    }
-    for(int i = 0; i < 16; i++) {
-        ED25519_REG(ED25519_REG_DATA_HRAM + i*4) = *(((uint32_t*)(hram)) + i);
-    }
-    for(int i = 0; i < 16; i++) {
-        ED25519_REG(ED25519_REG_DATA_HSM + i*4) = *(((uint32_t*)(r)) + i);
-    }
-    ED25519_REG(ED25519_REG_STATUS_3) = 1;
-    while(!(ED25519_REG(ED25519_REG_STATUS_3) & 0x4)); // Wait
-    for(int i = 0; i < 8; i++) {
-        sig[i+8] = ED25519_REG(ED25519_REG_DATA_SIGN + i*4);
-    }
+    sc_reduce(hram);
+    sc_muladd(signature + 32, hram, private_key, r);
 }
